95599d799a0dda735a846f72fb4da16db678f046
Haridas <haridasnykiel@gmail.com>
2018-02-20 21:49:54 +0000

    Working on the logic for the clock

--- clockLogic.js
+++ clockLogic.js
@@ -1,4 +1,43 @@
 $(document).ready(function(){
-console.log("DOM is ready")
-    
+  var workTime;
+  var breakTime;
+  setWorkTimer($('#work').html());
+
+  function setWorkTimer(timeInMins) {
+    $('#timer').text(timeInMins);
+  }
+
+  $('.add').click(function() {
+    AddToCounters('#'+ $(this).val(), '+');
+    AddTime($(this).val());
+  });
+
+  $('.subtract').click(function() {
+    AddToCounters('#'+ $(this).val(), '-');
+    AddTime($(this).val());
+  });
+
+  function AddToCounters(element, operator) {
+    if(parseFloat($(element).html()) > 0 || operator === '+') {
+      if(operator === '+') {
+        $(element).html((parseFloat($(element).html()) + 1).toFixed(2));
+      } else if(operator === '-') {
+        $(element).html((parseFloat($(element).html()) - 1).toFixed(2));
+      }
+    }
+  }
+
+  function AddTime( timeType) {
+    if(timeType === 'work') {
+      workTime = parseFloat($('#work').html()).toFixed(2);
+      setWorkTimer(workTime);
+    } else if(timeType === 'break') {
+      breakTime = parseFloat($('#break').html()).toFixed(2);
+    }
+    console.log(workTime);
+    console.log(breakTime);
+  }
+
+
+
 });
