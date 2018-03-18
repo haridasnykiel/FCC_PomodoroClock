90a95a84c58bb7a1b5da6413e35f93400d0eb952
Haridas <haridasnykiel@gmail.com>
2018-03-18 16:04:26 +0000

    Added the audio for to trigger when the timer reaches 0 and added a bug fix for the progress bar.

--- clockLogic.js
+++ clockLogic.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
 
   var countDownTimer;
   var colours = ["#870F77", "#8E5A87", "#56244F", "#DC98D3", "#9D53B7", "#D87DF9", "#5500ff", "#884dff",
-                  "#ff3377", "#ff99bb", "#b30000", "#ff9999"];
+  "#ff3377", "#ff99bb", "#b30000", "#ff9999"];
+  var totalTime;
+  var totalTimeInSeconds;
+  var remainingTimeInSeconds;
 
   $('.add').click(function() {
     AddToCounters('#'+ $(this).val(), '+');
@@ -16,10 +19,7 @@ $(document).ready(function(){
     $('#timerTitle').html('Lets Do Some Work');
     $('#timer').html('25.00');
     clearInterval(countDownTimer);
-    $("#progress_bar").animate({
-      width: "100%",
-      borderRadius: "0%"
-    });
+    ProgressBarAnimation("#B14CFE", "100", "0");
     $("#go").html('GO');
   });
 
@@ -29,26 +29,29 @@ $(document).ready(function(){
         $('#timerTitle').html('Work');
         $('#timer').html($('#work').html());
       }
-      var totalTime = parseFloat($('#work').html());
-      var totalTimeInSeconds = totalTime * 60;
-      var remainingTimeInSeconds = totalTimeInSeconds;
+
+      if($("#progress_bar").css( "background-color" ) == "rgb(177, 76, 254)") {
+        totalTime = parseFloat($('#work').html());
+        totalTimeInSeconds = totalTime * 60;
+        remainingTimeInSeconds = totalTimeInSeconds;
+      }
+
       countDownTimer = setInterval(function(){
         var timeleft = parseFloat(calculateCount($('#timer').html()));
         remainingTimeInSeconds -= 1;
         var getPercentage = calculatePercentageForProgressBar(totalTimeInSeconds, remainingTimeInSeconds);
-        $("#progress_bar").animate({
-            backgroundColor: colours[Math.round(GetRandomArrValue(colours))],
-            width: String(getPercentage) + "%",
-            borderRadius: String(getPercentage) + "%"
-          });
+        ProgressBarAnimation(colours[Math.round(GetRandomArrValue(colours))],String(getPercentage),
+        String(getPercentage));
         if(timeleft <= 0) {
           clearInterval(countDownTimer);
           setTimerAndTitle();
           $('#go').html('GO');
-          $("#progress_bar").animate({
-            width: "100%",
-            borderRadius: "0%"
-          });
+          ProgressBarAnimation("#B14CFE", "100", "0");
+          if($('#timerTitle').html() == 'Break') {
+            PlayAudio("./audio/martial arts1.wav");
+          } else {
+            PlayAudio("./audio/martial arts2.wav");
+          }
 
         }
       },1000);
@@ -58,6 +61,11 @@ $(document).ready(function(){
     RunTimer('#go');
   });
 
+  function PlayAudio(audioFilePath) {
+    var audio = new Audio(audioFilePath);
+    audio.play();
+  }
+
   function GetRandomArrValue(arr) {
     return Math.random() * (arr.length - 0) + 0;
   }
@@ -65,6 +73,14 @@ $(document).ready(function(){
   function calculatePercentageForProgressBar(totalTimeInSecs, remainingTimeInSecs) {
     var decrease = totalTimeInSecs-remainingTimeInSecs;
     return 100-(decrease/totalTimeInSecs*100);
+  }
+
+  function ProgressBarAnimation(backgroundColor, width, borderRadius) {
+    $("#progress_bar").animate({
+      backgroundColor: backgroundColor,
+      width: width + "%",
+      borderRadius: borderRadius + "%"
+    });
   }
 
   function setTimerAndTitle() {
